f091bc8908725203637b514a6c804550c2457b4c
cahyana78 <49891412+cahyana78@users.noreply.github.com>
2022-07-27 09:49:16 +0700

    Menambahkan menu

--- app/src/main/java/com/example/pertemuan04_vsga/MainActivity.java
+++ app/src/main/java/com/example/pertemuan04_vsga/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.pertemuan04_vsga;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -31,5 +36,27 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Halo Selamat Pagi", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    //munculkan menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    //perintah ketika menu ditekan
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu1)
+            startActivity(new Intent(this, Menu1.class));
+        else if(item.getItemId() == R.id.menu2)
+            startActivity(new Intent(this, Menu2.class));
+
+        return true;
     }
 }
