063f226b182b323d63f497dda77c8d436c820046
cahyana78 <49891412+cahyana78@users.noreply.github.com>
2022-07-27 09:27:37 +0700

    Menambahkan aksi pada button

--- app/src/main/java/com/example/pertemuan04_vsga/MainActivity.java
+++ app/src/main/java/com/example/pertemuan04_vsga/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.pertemuan04_vsga;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnOk = findViewById(R.id.ok);
+        final Button btnTekan = findViewById(R.id.tekan);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnOk.setBackgroundColor(Color.RED);
+            }
+        });
+
+        btnTekan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Halo Selamat Pagi", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
